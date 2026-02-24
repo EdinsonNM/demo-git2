7bef4b47406aefc85253bb75a2e402b522e9c4e3
Edinson Nuñez More <nmedinson@gmail.com>
2026-02-23 20:33:17 -0500

    agregar función potencia y ajustar impresión de resultados

--- demo2.py
+++ demo2.py
@@ -7,6 +7,9 @@ def dividir(a,b):
         return a / b
     else:
         return "No se puede dividir por cero"
+    
+def potencia(a,b):
+    return a ** b
 
 
 multiplicacion = multiplicar(3, 8)
@@ -14,3 +17,6 @@ print("La multiplicacion es:", multiplicacion)
 
 division = dividir(10, 2)
 print("La division es:", division)
+
+potencia = potencia(2, 3)
+print("La potencia es:", potencia)
